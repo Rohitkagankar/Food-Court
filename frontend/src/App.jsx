bbc35378a764e261ed7e0b165de943bf74d2bf50
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-09-14 14:10:59 +0530

    explore menu section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import {Route, Routes } from "react-router-dom";
 import Cart from "./pages/Cart/Cart";
